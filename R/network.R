fb473370683877ef01716e86f0aba137c4a4e8d6
Jonah Duckles <jonah@duckles.org>
2020-08-10 08:21:13 +1200

    value filter

--- R/network.R
+++ R/network.R
@@ -2,46 +2,62 @@ library(networkD3)
 library(stringr)
 library(dplyr)
 
-trade_net <- function(df, country="New Zealand", year=2017, var="quantity") { 
+trade_net <- function(df, country="New Zealand", year=2017, var="Quantity", min_val=0) { 
   
-  honey <- df 
+  # Prepare data for visualizing as a Sankey Diagram of import/export flow.
   
-  country_ids_exp <- honey %>% filter(year== {{ year }}) %>%  filter(element == switch(var,Quantity="Export Quantity", Value="Export Value") ) %>% 
-    filter(reporter_countries == {{ country }}) %>% filter(value > 0) %>% 
+  # Compute 0-based country ids for the Sankey Network Diagram
+  country_ids_exp <- df %>% 
+    filter(year== {{ year }}) %>%  
+    filter(element == switch(var,Quantity="Export Quantity", Value="Export Value") ) %>% 
+    filter(reporter_countries == {{ country }}) %>% 
+    filter(value > 0) %>% 
+    filter(value > min_val) %>% 
     select(partner_countries) %>% 
     distinct() %>% 
     mutate(country_id = group_indices(., partner_countries)) %>% 
     mutate(country_id = country_id - 1) %>% 
     rbind(c({{ country }}, nrow(.))) 
   
-  exports <- honey %>% 
-    filter(year == {{ year }}) %>% 
-    filter(element == switch(var,Quantity="Export Quantity", Value="Export Value") ) %>% 
-    filter(reporter_countries == {{ country }}) %>% filter(value > 0) %>% 
-    select(reporter_countries, partner_countries, value) %>% 
-    left_join(country_ids_exp) %>% 
-    mutate(country_id = as.numeric(country_id)) %>% 
-    rename(target = country_id) %>% 
-    mutate(source = nrow(.))  %>% 
-    select(source, target, value)
-  
-  country_ids_imp <- honey %>% filter(year== {{ year }}) %>%  filter(element == switch(var,Quantity="Import Quantity", Value="Import Value")) %>% 
-    filter(reporter_countries == {{ country }}) %>% filter(value > 0) %>% 
+  # Start numbering export countries from last row of export countries 
+  country_ids_imp <- df %>% 
+    filter(year == {{ year }}) %>%  
+    filter(element == switch(var,Quantity="Import Quantity", Value="Import Value")) %>% 
+    filter(reporter_countries == {{ country }}) %>% 
+    filter(value > 0) %>% 
+    filter(value > min_val) %>% 
     select(partner_countries) %>% 
     distinct() %>% 
     mutate(country_id = group_indices(., partner_countries) + nrow(country_ids_exp) ) %>% 
     mutate(country_id = country_id - 1)
   
-  imports <- honey %>% 
+  exports <- df %>% 
+    filter(year == {{ year }}) %>% 
+    filter(element == switch(var,Quantity="Export Quantity", Value="Export Value") ) %>% 
+    filter(reporter_countries == {{ country }}) %>% 
+    filter(value > 0) %>% 
+    filter(value > min_val) %>% 
+    select(reporter_countries, partner_countries, value) %>% 
+    left_join(country_ids_exp) %>% 
+    mutate(country_id = as.numeric(country_id)) %>% 
+    rename(target = country_id) %>% 
+    mutate(source = nrow(.))  %>% 
+    select(source, target, value) %>% 
+    arrange(value)
+  
+  imports <- df %>% 
     filter(year ==  {{ year }}) %>% 
     filter(element == switch(var,Quantity="Import Quantity", Value="Import Value")) %>% 
-    filter(reporter_countries == {{ country }}) %>% filter(value > 0) %>% 
+    filter(reporter_countries == {{ country }}) %>% 
+    filter(value > 0) %>% 
+    filter(value > min_val) %>% 
     select(reporter_countries, partner_countries, value) %>% 
     left_join(country_ids_imp) %>% 
     mutate(country_id = as.numeric(country_id) ) %>% 
     rename(source = country_id) %>% 
     mutate(target = nrow(country_ids_exp) - 1)  %>% 
-    select(source, target, value)
+    select(source, target, value) %>% 
+    arrange(value)
   
   country_ids <- rbind(country_ids_exp, country_ids_imp)
   trade <- rbind(exports, imports) 
@@ -57,6 +73,7 @@ plot_sankey <- function(network) {
                 Target = 'target', 
                 Value = 'value', 
                 NodeID = 'partner_countries',
-                fontSize=18, 
+                fontSize=20, 
                 fontFamily="sans-serif")
 }
+
