463b4c2c375bce543f79b79305fc9db416d19540
Jonah Duckles <jonah@duckles.org>
2020-08-10 08:20:54 +1200

    Value filter

--- app.R
+++ app.R
@@ -17,6 +17,7 @@ library(janitor)
 
 honey <- janitor::clean_names(read_csv('FAOSTAT_data_6-5-2020.csv') )
 
+
 reporting_countries <- honey %>% distinct(reporter_countries)
 
 variables <- c("Quantity", "Value") 
@@ -38,7 +39,9 @@ ui <- fluidPage(
         sidebarPanel(
            selectInput("country", "Select Reporting Country", choices = reporting_countries),
            selectInput("measure", "Which measure?", choices = variables),
+           sliderInput("quantity_filter", "Filter minimum quantity", min=0, max=10000, step=1, value=0), 
            sliderInput("year", "Years", min=min_year, max=max_year, step=1, value=max_year, sep="" )
+          # plotlyOutput("world", "World Honey") 
         ),
 
         # Show a plot of the generated distribution
@@ -57,24 +60,34 @@ ui <- fluidPage(
 
 server <- function(input, output) {
 
-    # subset_data <- reactive({
-    #     honey %>% 
-    #         filter(`Reporter Countries` == input$country ) %>% 
-    #         filter(`Element` == input$measure) %>% 
-    #         filter(`Year` > input$years[1] ) %>% 
-    #         filter(`Year` < input$years[2] ) %>% 
-    #         select(Element, Year, `Partner Countries`, Value, Unit)
-    # 
-    #     
-    #     })
+    subset_data <- reactive({
+      
+      subset_trade <- function(df, country, measure, year, value_filter) {
+         df %>% 
+             filter(reporter_countries == input$country ) %>% 
+             filter(element == input$measure) %>% 
+             filter(year == input$years) %>% 
+             filter(value > input$value_filter) %>% 
+             select(element, year, partner_countries, value, unit)
+      }
+      
+      subset_trade(honey, input$country, input$measure, input$years, input$value_filter)
+      
+    })
     
-    # output$tabledata <- renderDataTable({
-    #   subset_data()   
-    # })
-    
+    output$world <- renderPlot({
+      
+      
+      ggplot2( honey, aes())
+    })
+  
     output$sankey_plot <- renderSankeyNetwork({
      
-      plot_sankey(trade_net(honey, country=input$country, year=input$year, var=input$measure))
+      plot_sankey(trade_net(honey, 
+                            country=input$country, 
+                            year=input$year, 
+                            var=input$measure, 
+                            min_val=input$quantity_filter))
      
     })
     
