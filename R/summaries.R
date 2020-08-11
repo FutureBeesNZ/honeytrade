d26f58afd5e7eeb60f6e0b933025663d0496d1e8
Jonah Duckles <jonah@duckles.org>
2020-08-12 10:54:43 +1200

    Summarising top commodities

---
+++ R/summaries.R
@@ -0,0 +1,21 @@
+
+top_commodities <- function(country, element="expq", year=2017, n=20)  {
+
+  element <- switch( element, 
+          expv="Export Value",
+          expq="Export Quantity",
+          impv="Import Value",
+          impq="Import Quantity"
+          )
+          
+  
+  trade_matrix %>% 
+    filter(country == {{ country }}) %>% 
+    filter(year == {{ year }}) %>% 
+    filter(element == {{ element }}) %>% 
+    group_by(item) %>% 
+    summarise(total = sum(value)) %>% 
+    arrange(desc(total)) %>% 
+    head(n) 
+    
+}
