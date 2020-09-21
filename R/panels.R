f075c905ff609d6a71697769728e4ff516d956da
Jonah Duckles <jonah@duckles.org>
2020-09-21 13:27:52 +1200

    Testing update for guidance.

--- R/panels.R
+++ R/panels.R
@@ -39,7 +39,7 @@ sankeyPanelUI <- function(id, panel_name="Sankey Diagrams")  {
           pickerInput(ns("commodity"), "Which commodity?", choices= commodities, selected = "Honey, natural", options=pickerOptions(liveSearch=TRUE)), 
           pickerInput(ns("country"), "Select Reporting Country", choices = reporting_countries, selected = "New Zealand", options=pickerOptions(liveSearch=TRUE)),
           pickerInput(ns("measure"), "Which measure?", choices = variables),
-          sliderInput(ns("quantity_filter"), "Filter minimum quantity", min=0, max=10000, step=1, value=0), 
+          sliderInput(ns("quantity_filter"), paste("Filter minimum quantity", span("IF THE PLOT LOOKS STRANGE, TRY FILTERING", style="color:red"), sep=" "), min=0, max=10000, step=1, value=0), 
           sliderInput(ns("year"), "Years", min=min_year, max=max_year, step=1, value=max_year, sep="" ),
           tableOutput(ns("exporters")),
           tableOutput(ns("importers"))
